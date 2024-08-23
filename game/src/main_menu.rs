9bb16c35ced42a4fb9b845b19bc1789b3e946fd2
hollowdll <pixerchat@gmail.com>
2024-08-23 21:47:13 +0300

    add menu for new game

--- game/src/main_menu.rs
+++ game/src/main_menu.rs
@@ -2,7 +2,7 @@ use crossterm::{
     cursor::{self, Hide, Show},
     event::{self, Event, KeyCode, KeyEvent},
     execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen},
 };
 use std::io;
 
@@ -29,6 +29,7 @@ fn print_ascii_title(mut out: &io::Stdout) -> io::Result<()> {
     Ok(())
 }
 
+/// Returned bool is true if the menu should be rerendered.
 pub fn main_menu() -> io::Result<bool> {
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, Hide)?;
@@ -36,7 +37,7 @@ pub fn main_menu() -> io::Result<bool> {
     let menu_items = vec![OPTION_LOAD_GAME, OPTION_NEW_GAME, OPTION_QUIT_GAME];
     let mut selected_index = 0;
     let start_column: u16 = 7;
-    let mut render_again = false;
+    let mut rerender = false;
 
     loop {
         execute!(stdout, cursor::MoveTo(0, 0))?;
@@ -74,24 +75,31 @@ pub fn main_menu() -> io::Result<bool> {
     match menu_items[selected_index] {
         OPTION_LOAD_GAME => {
             execute!(stdout, LeaveAlternateScreen, Show)?;
-            if let Ok(pressed_back) = load_game() {
+            if let Ok(pressed_back) = menu_load_game() {
                 if pressed_back {
-                    render_again = true;
+                    rerender = true;
                 }
             }
         }
-        OPTION_NEW_GAME => {}
+        OPTION_NEW_GAME => {
+            execute!(stdout, LeaveAlternateScreen, Show)?;
+            if let Ok(pressed_back) = menu_new_game() {
+                if pressed_back {
+                    rerender = true;
+                }
+            }
+        }
         OPTION_QUIT_GAME => {}
         _ => {}
     }
 
     execute!(stdout, LeaveAlternateScreen, Show)?;
 
-    Ok(render_again)
+    Ok(rerender)
 }
 
 /// Returns true if menu option "Back" was pressed.
-fn load_game() -> io::Result<bool> {
+fn menu_load_game() -> io::Result<bool> {
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, Hide)?;
 
@@ -101,7 +109,59 @@ fn load_game() -> io::Result<bool> {
 
     loop {
         execute!(stdout, cursor::MoveTo(0, 0))?;
-        println!("No save files found");
+        println!("No characters found");
+        execute!(stdout, cursor::MoveTo(0, 1))?;
+
+        for (i, item) in menu_items.iter().enumerate() {
+            execute!(stdout, cursor::MoveTo(0, i as u16 + start_column))?;
+            if i == selected_index {
+                println!("> {}", item);
+            } else {
+                println!("  {}", item);
+            }
+        }
+
+        if let Event::Key(KeyEvent { code, .. }) = event::read()? {
+            match code {
+                KeyCode::Up => {
+                    if selected_index > 0 {
+                        selected_index -= 1;
+                    }
+                }
+                KeyCode::Down => {
+                    if selected_index < menu_items.len() - 1 {
+                        selected_index += 1;
+                    }
+                }
+                KeyCode::Enter => {
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+
+    match menu_items[selected_index] {
+        "Back" => {}
+        _ => {}
+    }
+
+    execute!(stdout, LeaveAlternateScreen, Show)?;
+
+    Ok(true)
+}
+
+fn menu_new_game() -> io::Result<bool> {
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, Hide)?;
+
+    let menu_items = vec!["Back"];
+    let mut selected_index = 0;
+    let start_column: u16 = 1;
+
+    loop {
+        execute!(stdout, cursor::MoveTo(0, 0))?;
+        println!("Create a new character");
         execute!(stdout, cursor::MoveTo(0, 1))?;
 
         for (i, item) in menu_items.iter().enumerate() {
