e3d70fd51c0b9a702a4ea741bd4737fa7e5329b8
hollowdll <pixerchat@gmail.com>
2024-09-01 19:34:04 +0300

    dungeon room structures

--- game/src/dungeon.rs
+++ game/src/dungeon.rs
@@ -1,26 +1,68 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 pub struct DungeonFloor {
     pub floor: u32,
+    pub start_room: Rc<RefCell<Room>>,
 }
 
 impl DungeonFloor {
-    pub fn new(floor: u32) -> Self {
-        Self { floor }
+    pub fn new(floor: u32, start_room: Rc<RefCell<Room>>) -> Self {
+        Self { floor, start_room }
     }
 }
 
-pub struct DungeonRoom {
-    pub kind: DungeonRoomKind,
+pub struct Room {
+    pub kind: RoomKind,
+    pub adjacent_rooms: AdjacentRooms,
 }
 
-pub enum DungeonRoomKind {
+pub struct AdjacentRooms {
+    pub up: Option<Rc<RefCell<Room>>>,
+    pub down: Option<Rc<RefCell<Room>>>,
+    pub left: Option<Rc<RefCell<Room>>>,
+    pub right: Option<Rc<RefCell<Room>>>,
+}
+
+impl Room {
+    pub fn new(kind: RoomKind) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Room {
+            kind,
+            adjacent_rooms: AdjacentRooms {
+                up: None,
+                down: None,
+                left: None,
+                right: None,
+            },
+        }))
+    }
+}
+
+pub enum RoomKind {
     Start,
-    Boss,
-    OneWay,
-    TwoWay,
-    ThreeWay,
+    BossEntrance,
+    OneWayUp,
+    OneWayDown,
+    OneWayLeft,
+    OneWayRight,
+    TreasureUp,
+    TreasureDown,
+    TreasureLeft,
+    TreasureRight,
+    TwoWayUpDown,
+    TwoWayLeftDown,
+    TwoWayUpLeft,
+    TwoWayUpRight,
+    TwoWayDownLeft,
+    TwoWayDownRight,
+    ThreeWayDownLeftRight,
+    ThreeWayUpLeftRight,
+    ThreeWayUpDownLeft,
+    ThreeWayUpDownRight,
     FourWay,
 }
 
 pub fn generate_random_dungeon_floor(floor: u32) -> DungeonFloor {
-    return DungeonFloor::new(floor);
+    let start_room = Room::new(RoomKind::Start);
+    return DungeonFloor::new(floor, start_room);
 }
