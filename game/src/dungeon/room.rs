a0836a5ad4a8896aefb5d094d8bc4047cd9861a6
hollowdll <pixerchat@gmail.com>
2024-09-14 15:49:43 +0300

    dungeon room display functions

--- game/src/dungeon/room.rs
+++ game/src/dungeon/room.rs
@@ -24,7 +24,30 @@ pub fn display_start_room(start_column: u16) -> io::Result<u16> {
     Ok(start_column + 8)
 }
 
-pub fn display_twowayupdown_room(start_column: u16) -> io::Result<u16> {
+pub fn display_boss_entrance_room(start_column: u16) -> io::Result<u16> {
+    let mut stdout = io::stdout();
+    execute!(stdout, cursor::MoveTo(0, start_column))?;
+    println!("");
+    execute!(stdout, cursor::MoveTo(0, start_column + 1))?;
+    println!("+-------------+");
+    execute!(stdout, cursor::MoveTo(0, start_column + 2))?;
+    println!("|    | B |    |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 3))?;
+    println!("|             |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 4))?;
+    println!("|             |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 5))?;
+    println!("|             |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 6))?;
+    println!("+----     ----+");
+    execute!(stdout, cursor::MoveTo(0, start_column + 7))?;
+    println!("");
+    execute!(stdout, cursor::MoveTo(0, start_column + 8))?;
+
+    Ok(start_column + 8)
+}
+
+pub fn display_two_way_up_down_room(start_column: u16) -> io::Result<u16> {
     let mut stdout = io::stdout();
     execute!(stdout, cursor::MoveTo(0, start_column))?;
     println!("");
@@ -36,6 +59,121 @@ pub fn display_twowayupdown_room(start_column: u16) -> io::Result<u16> {
     println!("|             |");
     execute!(stdout, cursor::MoveTo(0, start_column + 4))?;
     println!("|             |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 5))?;
+    println!("|             |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 6))?;
+    println!("+----     ----+");
+    execute!(stdout, cursor::MoveTo(0, start_column + 7))?;
+    println!("");
+    execute!(stdout, cursor::MoveTo(0, start_column + 8))?;
+
+    Ok(start_column + 8)
+}
+
+pub fn display_two_way_left_right_room(start_column: u16) -> io::Result<u16> {
+    let mut stdout = io::stdout();
+    execute!(stdout, cursor::MoveTo(0, start_column))?;
+    println!("");
+    execute!(stdout, cursor::MoveTo(0, start_column + 1))?;
+    println!("+-------------+");
+    execute!(stdout, cursor::MoveTo(0, start_column + 2))?;
+    println!("|             |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 3))?;
+    println!("               ");
+    execute!(stdout, cursor::MoveTo(0, start_column + 4))?;
+    println!("               ");
+    execute!(stdout, cursor::MoveTo(0, start_column + 5))?;
+    println!("|             |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 6))?;
+    println!("+-------------+");
+    execute!(stdout, cursor::MoveTo(0, start_column + 7))?;
+    println!("");
+    execute!(stdout, cursor::MoveTo(0, start_column + 8))?;
+
+    Ok(start_column + 8)
+}
+
+pub fn display_two_way_up_left_room(start_column: u16) -> io::Result<u16> {
+    let mut stdout = io::stdout();
+    execute!(stdout, cursor::MoveTo(0, start_column))?;
+    println!("");
+    execute!(stdout, cursor::MoveTo(0, start_column + 1))?;
+    println!("+----     ----+");
+    execute!(stdout, cursor::MoveTo(0, start_column + 2))?;
+    println!("|             |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 3))?;
+    println!("              |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 4))?;
+    println!("              |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 5))?;
+    println!("|             |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 6))?;
+    println!("+-------------+");
+    execute!(stdout, cursor::MoveTo(0, start_column + 7))?;
+    println!("");
+    execute!(stdout, cursor::MoveTo(0, start_column + 8))?;
+
+    Ok(start_column + 8)
+}
+
+pub fn display_two_way_up_right_room(start_column: u16) -> io::Result<u16> {
+    let mut stdout = io::stdout();
+    execute!(stdout, cursor::MoveTo(0, start_column))?;
+    println!("");
+    execute!(stdout, cursor::MoveTo(0, start_column + 1))?;
+    println!("+----     ----+");
+    execute!(stdout, cursor::MoveTo(0, start_column + 2))?;
+    println!("|             |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 3))?;
+    println!("|              ");
+    execute!(stdout, cursor::MoveTo(0, start_column + 4))?;
+    println!("|              ");
+    execute!(stdout, cursor::MoveTo(0, start_column + 5))?;
+    println!("|             |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 6))?;
+    println!("+-------------+");
+    execute!(stdout, cursor::MoveTo(0, start_column + 7))?;
+    println!("");
+    execute!(stdout, cursor::MoveTo(0, start_column + 8))?;
+
+    Ok(start_column + 8)
+}
+
+pub fn display_two_way_down_left_room(start_column: u16) -> io::Result<u16> {
+    let mut stdout = io::stdout();
+    execute!(stdout, cursor::MoveTo(0, start_column))?;
+    println!("");
+    execute!(stdout, cursor::MoveTo(0, start_column + 1))?;
+    println!("+-------------+");
+    execute!(stdout, cursor::MoveTo(0, start_column + 2))?;
+    println!("|             |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 3))?;
+    println!("              |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 4))?;
+    println!("              |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 5))?;
+    println!("|             |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 6))?;
+    println!("+----     ----+");
+    execute!(stdout, cursor::MoveTo(0, start_column + 7))?;
+    println!("");
+    execute!(stdout, cursor::MoveTo(0, start_column + 8))?;
+
+    Ok(start_column + 8)
+}
+
+pub fn display_two_way_down_right_room(start_column: u16) -> io::Result<u16> {
+    let mut stdout = io::stdout();
+    execute!(stdout, cursor::MoveTo(0, start_column))?;
+    println!("");
+    execute!(stdout, cursor::MoveTo(0, start_column + 1))?;
+    println!("+-------------+");
+    execute!(stdout, cursor::MoveTo(0, start_column + 2))?;
+    println!("|             |");
+    execute!(stdout, cursor::MoveTo(0, start_column + 3))?;
+    println!("|              ");
+    execute!(stdout, cursor::MoveTo(0, start_column + 4))?;
+    println!("|              ");
     execute!(stdout, cursor::MoveTo(0, start_column + 5))?;
     println!("|             |");
     execute!(stdout, cursor::MoveTo(0, start_column + 6))?;
