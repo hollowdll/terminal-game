950e4dae1e666f4209cbe17680c3bd0cea42e7a5
hollowdll <pixerchat@gmail.com>
2024-09-14 15:08:52 +0300

    confirm return back to main menu

--- game/src/menu/character.rs
+++ game/src/menu/character.rs
@@ -62,12 +62,70 @@ pub fn menu_character(character: &mut PlayerCharacter) -> io::Result<bool> {
                     "Equipment" => {
                         menu_equipment(character)?;
                     }
-                    "Return to main menu" => return Ok(true),
+                    "Return to main menu" => {
+                        let confirm = menu_confirm_return_to_main_menu()?;
+                        if confirm {
+                            return Ok(true);
+                        }
+                        execute!(stdout, Clear(ClearType::All))?;
+                    }
                     _ => break,
                 },
                 _ => {}
             }
         }
+    }
+
+    Ok(false)
+}
+
+fn menu_confirm_return_to_main_menu() -> io::Result<bool> {
+    let mut stdout = io::stdout();
+    execute!(stdout, Clear(ClearType::All))?;
+
+    let menu_items = vec!["No", "Yes"];
+    let mut selected_index = 0;
+    let start_column: u16 = 2;
+
+    loop {
+        execute!(stdout, cursor::MoveTo(0, 0))?;
+        println!("Return back to the game main menu?");
+        execute!(stdout, cursor::MoveTo(0, 1))?;
+        println!("Progress in the current dungeon floor will be lost.");
+        execute!(stdout, cursor::MoveTo(0, 2))?;
+
+        for (i, item) in menu_items.iter().enumerate() {
+            execute!(stdout, cursor::MoveTo(0, i as u16 + start_column))?;
+            if i == selected_index {
+                println!("> {}", item);
+            } else {
+                println!("  {}", item);
+            }
+        }
+
+        if let Event::Key(KeyEvent { code, .. }) = event::read()? {
+            match code {
+                KeyCode::Up => {
+                    if selected_index > 0 {
+                        selected_index -= 1;
+                    }
+                }
+                KeyCode::Down => {
+                    if selected_index < menu_items.len() - 1 {
+                        selected_index += 1;
+                    }
+                }
+                KeyCode::Enter => {
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+
+    match menu_items[selected_index] {
+        "Yes" => return Ok(true),
+        _ => {}
     }
 
     Ok(false)
