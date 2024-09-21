7e24a94a420c571e762b478fc47ce67c8d485f0d
hollowdll <pixerchat@gmail.com>
2024-09-21 16:29:15 +0300

    add shop item purchase functions

--- game/src/shop.rs
+++ game/src/shop.rs
@@ -1,26 +1,84 @@
 use crate::{
     items::{
         generate_random_armor, generate_random_consumable, generate_random_ring,
-        generate_random_weapon, get_item_display_name, get_item_sell_value, ArmorItem,
-        CharacterItem, ConsumableItem, RingItem, WeaponItem, ARMOR_BASE_VALUES, RING_BASE_VALUES,
-        WEAPON_BASE_VALUES,
+        generate_random_weapon, get_item_display_name, get_item_purchase_value,
+        get_item_sell_value, ArmorItem, CharacterItem, ConsumableItem, RingItem, WeaponItem,
+        ARMOR_BASE_VALUES, RING_BASE_VALUES, WEAPON_BASE_VALUES,
     },
     session::PlayerCharacter,
 };
 
 pub struct ShopItems {
-    pub consumable: ConsumableItem,
-    pub weapon: WeaponItem,
-    pub armor: ArmorItem,
-    pub ring: RingItem,
+    pub consumable: Option<ConsumableItem>,
+    pub weapon: Option<WeaponItem>,
+    pub armor: Option<ArmorItem>,
+    pub ring: Option<RingItem>,
+}
+
+impl ShopItems {
+    /// Returns true if player has enough gold and the item was bought.
+    pub fn buy_consumable(&mut self, character: &mut PlayerCharacter) -> bool {
+        if let Some(item) = &self.consumable {
+            let purchase_value = get_item_purchase_value(&item.rarity);
+            if character.data.currency.gold >= purchase_value {
+                character.give_consumable(&item, 1);
+                character.data.currency.gold -= purchase_value;
+                self.consumable = None;
+                return true;
+            }
+        }
+        false
+    }
+
+    /// Returns true if player has enough gold and the item was bought.
+    pub fn buy_weapon(&mut self, character: &mut PlayerCharacter) -> bool {
+        if let Some(item) = &self.weapon {
+            let purchase_value = get_item_purchase_value(&item.rarity);
+            if character.data.currency.gold >= purchase_value {
+                character.give_weapon(&item);
+                character.data.currency.gold -= purchase_value;
+                self.weapon = None;
+                return true;
+            }
+        }
+        false
+    }
+
+    /// Returns true if player has enough gold and the item was bought.
+    pub fn buy_armor(&mut self, character: &mut PlayerCharacter) -> bool {
+        if let Some(item) = &self.armor {
+            let purchase_value = get_item_purchase_value(&item.rarity);
+            if character.data.currency.gold >= purchase_value {
+                character.give_armor(&item);
+                character.data.currency.gold -= purchase_value;
+                self.armor = None;
+                return true;
+            }
+        }
+        false
+    }
+
+    /// Returns true if player has enough gold and the item was bought.
+    pub fn buy_ring(&mut self, character: &mut PlayerCharacter) -> bool {
+        if let Some(item) = &self.ring {
+            let purchase_value = get_item_purchase_value(&item.rarity);
+            if character.data.currency.gold >= purchase_value {
+                character.give_ring(&item);
+                character.data.currency.gold -= purchase_value;
+                self.ring = None;
+                return true;
+            }
+        }
+        false
+    }
 }
 
 pub fn randomize_shop_items(floor: u32) -> ShopItems {
     ShopItems {
-        consumable: generate_random_consumable(),
-        weapon: generate_random_weapon(WEAPON_BASE_VALUES, floor),
-        armor: generate_random_armor(ARMOR_BASE_VALUES, floor),
-        ring: generate_random_ring(RING_BASE_VALUES, floor),
+        consumable: Some(generate_random_consumable()),
+        weapon: Some(generate_random_weapon(WEAPON_BASE_VALUES, floor)),
+        armor: Some(generate_random_armor(ARMOR_BASE_VALUES, floor)),
+        ring: Some(generate_random_ring(RING_BASE_VALUES, floor)),
     }
 }
 
